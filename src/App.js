7cc6118beec8087c5dcf73ed843de0fbd6546b3d
Ruslan Golovach <ruslan.golovach@ukr.net>
2021-06-26 14:09:06 +0300

    оформил дз 4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import styles from './App.module.css';
+import AppBar from './components/AppBar';
+import HomePage from './views/HomePage';
+import MoviesPage from './views/MoviesPage';
+import MovieDetailsPage from './views/MovieDetailsPage';
+import routes from './routes';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <AppBar />
+      <Switch>
+        <Route exact path={routes.home} component={HomePage} />
+        <Route exact path={routes.movies} component={MoviesPage} />
+        <Route path={routes.movieDetails} component={MovieDetailsPage} />
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Reviews/Reviews.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import moviesAPI from '../../services/moviesAPI';
+
+class Reviews extends Component {
+  state = {
+    reviews: null,
+  };
+
+  async componentDidMount() {
+    const { id } = this.props;
+
+    const response = await moviesAPI.fetchReviews(id);
+    this.setState({ reviews: response.data.results });
+  }
+
+  render() {
+    const { reviews } = this.state;
+
+    return (
+      <ul>
+        {reviews && reviews.length > 0
+          ? reviews.map(({ id, author, content, url }) => (
+              <li key={id}>
+                <h2>{author}</h2>
+                <p>{content.slice(0, 500)}</p>
+                <a href={url} target="_blank" rel="noopener noreferrer">
+                  ReadMore
+                </a>
+              </li>
+            ))
+          : 'Sorry, no info'}
+      </ul>
+    );
+  }
+}
+
+export default Reviews;
